099ff9593819f9b522da7b6f87bb35e82ae7fc86
yvainepanyan_i <yvainepanyan_i@didiglobal.com>
2020-10-25 08:59:49 +0800

    交换链表节点

--- Python_DataStructure/venv/linklist/swap_node.py
+++ Python_DataStructure/venv/linklist/swap_node.py
@@ -23,13 +23,46 @@ class LinkedList:
         self.head = new_node
 
     def swapNodes(self,d1,d2):
-        prevD1 = d1
-        prevD2 = d2
+        prevD1 = None
+        prevD2 = None
         #假设这两个
         if d1 == d2:
             return
         else:
-            D1 =
+            D1 = self.head
+            while D1 is not None and D1.data != d1:
+                prevD1 = D1
+                D1 = D1.next
+            D2 = self.head
+            while D2 is not None and D2.data != d2:
+                prevD2 = D2
+                D2 = D2.next
+            if D1 is None and D2 is None:
+                return
+            if prevD1 is not None:
+                prevD1.next = D2
+            else:
+                self.head = D2
+            if prevD2 is not None:
+                prevD2.next = D1
+            else:
+                self.head = D1
+            temp = D1.next
+            D1.next = D2.next
+            D2.next = temp
+
+if __name__ == '__main__':
+    list = LinkedList()
+    list.insert(5)
+    list.insert(4)
+    list.insert(3)
+    list.insert(2)
+    list.insert(1)
+    list.print_list()
+    list.swapNodes(1,4)
+    print("after swapping")
+    list.print_list()
+
 
 
 
